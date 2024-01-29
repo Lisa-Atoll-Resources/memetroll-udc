f1bacda3dd844d520593e5dab074246c14f74ade
“OnFire-Nasir” <nasironfire07@gmail.com>
2024-01-29 08:16:30 +0000

    Initial

---
+++ genesis_miatoll.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2024 GenesisOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from miatoll device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit common genesisExtended stuff.
+$(call inherit-product, vendor/genesis/config/common.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device
+PRODUCT_NAME := genesis_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := SM6250
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit some common device props
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
